e5d909a1e3be66e0f64d2c0a5efbb7e499dc25e9
bob <dlee67@msudenver.edu>
2017-07-17 20:03:00 -0600

    08:02PM

--- ProjectOneArduinoWorkshop/ProjectOneArduinoWorkshop.ino
+++ ProjectOneArduinoWorkshop/ProjectOneArduinoWorkshop.ino
@@ -1,31 +1,65 @@
-void setup() {
+//About the PWM
+/*
+ * Am not entirely sure how this thing works....
+ */
+int d = 5;
+void setup(){
+  pinMode(3, OUTPUT);
+}
 
+void loop(){
+  for(int a = 0; a < 256; a++){
+    analogWrite(3, a);
+    delay(d);  
+  }
+
+  for(int a = 255; a >= 0; a--){
+    analogWrite(3, a);
+    delay(d);  
+  }
+  delay(200);
+}
+/*
+  void setup(){
   pinMode(2, OUTPUT);
- 
+  }
+
+  void loop(){
+    digitalWrite(2, HIGH);
+    delay(500);
+    digitalWrite(2, LOW);
+  }
+*/
+/*
+void setup() {
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(2, HIGH);
-  delay(500);
-  digitalWrite(2, LOW);
-  
+//Testing if delay works like a sleep().
+  delay(1000);
+  for(int c = 5; c > 0; c--){
+    digitalWrite(2, HIGH);
+    delay(c*100);
+    digitalWrite(2, LOW);
+    delay(c*100);
+  }    
 }
-
+*/
 /*
- * Sequentially, lights up three LED light bulbds in a "wavy way".
- * This project was inspired by project 1 in 
- * Was done during the 07/15/17.
- */
+   Sequentially, lights up three LED light bulbds in a "wavy way".
+   This project was inspired by project 1 in
+   Was done during the 07/15/17.
+*/
 /*
-void setup() {
-  pinMode(2, OUTPUT); 
+  void setup() {
+  pinMode(2, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(6, OUTPUT);
 
-}
+  }
 
-void loop() {
+  void loop() {
   digitalWrite(2, HIGH);
   delay(500);
   digitalWrite(2, LOW);
@@ -34,6 +68,6 @@ void loop() {
   digitalWrite(4, LOW);
   digitalWrite(6, HIGH);
   delay(500);
-  digitalWrite(6, LOW);  
-}
+  digitalWrite(6, LOW);
+  }
 */
